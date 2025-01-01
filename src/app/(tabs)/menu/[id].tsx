5c9e4bc1e46d97fde0cd506efda2a6926d136783
samaila-bashir <developer.chatto@gmail.com>
2025-01-01 01:51:39 +0100

    implemented layout for select size on product details page

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,16 +1,90 @@
 import { Stack, useLocalSearchParams } from "expo-router";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import products from "@assets/data/products";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+import { useState } from "react";
+
+const sizes = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
+  const [selectedSize, setSelectedSize] = useState(sizes[1]);
+
+  const product = products.find((product) => product.id.toString() === id);
+
+  if (!product) {
+    return <Text>Product not found</Text>;
+  }
 
   return (
-    <View>
-      <Stack.Screen options={{ title: "Details" }} />
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
 
-      <Text>Product Details Screen for ID: {id}</Text>
+      <Text>Select size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((size) => (
+          <Pressable
+            onPress={() => setSelectedSize(size)}
+            key={size}
+            style={[
+              styles.size,
+              {
+                backgroundColor: size === selectedSize ? "gainsboro" : "white",
+              },
+            ]}
+          >
+            <Text
+              style={[
+                styles.sizeText,
+                { color: selectedSize === size ? "black" : "gray" },
+              ]}
+            >
+              {size}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
+
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+  size: {
+    backgroundColor: "gainsboro",
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+});
 
 export default ProductDetailsScreen;
